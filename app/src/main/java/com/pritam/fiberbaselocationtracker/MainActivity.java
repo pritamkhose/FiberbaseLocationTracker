2b29c60cdb2784748064ac6cabe60518684412a5
Pritam <pritamkhose@gmail.com>
2018-05-27 09:12:52 +0530

    v 1.0

--- app/src/main/java/com/pritam/fiberbaselocationtracker/MainActivity.java
+++ app/src/main/java/com/pritam/fiberbaselocationtracker/MainActivity.java
@@ -1,25 +1,38 @@
 package com.pritam.fiberbaselocationtracker;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.pritam.fiberbaselocationtracker.services.LocationMonitoringService;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -37,11 +50,23 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+// http://devdeeds.com/android-location-tracking-in-background-service/
+
+
 public class MainActivity extends AppCompatActivity {
 
     Button http_get, http_post;
     TextView text_response;
     ProgressDialog progress;
+    Switch gpsswitch;
+//    private RequestPermissionHandler mRequestPermissionHandler;
+private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+
+    private boolean mAlreadyStartedService = false;
+
+  
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +75,16 @@ public class MainActivity extends AppCompatActivity {
 
 
         text_response = (TextView) findViewById(R.id.text_response);
+        gpsswitch = (Switch) findViewById(R.id.gpsswitch);
+        gpsswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.v("Switch State=", ""+isChecked);
+                startStopService(isChecked);
+            }
+
+        });
 
         http_get = (Button) findViewById(R.id.http_get);
         http_get.setOnClickListener(new View.OnClickListener() {
@@ -79,7 +114,26 @@ public class MainActivity extends AppCompatActivity {
         progress.setMessage("Wait while loading...");
         progress.setCancelable(false);
 
+
+        //mRequestPermissionHandler = new RequestPermissionHandler();
+        //handlePermission();
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        String latitude = intent.getStringExtra(LocationMonitoringService.EXTRA_LATITUDE);
+                        String longitude = intent.getStringExtra(LocationMonitoringService.EXTRA_LONGITUDE);
+
+                        if (latitude != null && longitude != null) {
+                            text_response.setText(getString(R.string.msg_location_service_started) + "\n Latitude : " + latitude + "\n Longitude: " + longitude);
+                        }
+                    }
+                }, new IntentFilter(LocationMonitoringService.ACTION_LOCATION_BROADCAST)
+        );
     }
+
+
 
     private boolean isNetworkAvaiable() {
         ConnectivityManager cm = (ConnectivityManager) getApplicationContext()
@@ -127,14 +181,14 @@ public class MainActivity extends AppCompatActivity {
                     progress.dismiss();
                     myResponse = response.toString() + "\n";
 
-                    try {
-                        //JsonObject newObj = new JsonParser().parse(response.body().toString()).getAsJsonObject();
-                        JsonArray newArr = new JsonParser().parse(response.body().toString()).getAsJsonArray();
-                        myResponse += String.valueOf(newArr);
-                    } catch (Exception e) {
-                        Log.d("-->>", getStackTrace(e));
+//                    try {
+//                        //JsonObject newObj = new JsonParser().parse(response.body().toString()).getAsJsonObject();
+//                        JsonArray newArr = new JsonParser().parse(response.body().toString()).getAsJsonArray();
+//                        myResponse += String.valueOf(newArr);
+//                    } catch (Exception e) {
+//                        Log.d("-->>", getStackTrace(e));
                         myResponse += response.body().toString();
-                    }
+//                    }
 
 
                     Log.d("-->>", myResponse);
@@ -257,4 +311,294 @@ public class MainActivity extends AppCompatActivity {
         body.add(hm);
         return gson.toJson(body);
     }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        checkGooglePlayServices();
+    }
+
+
+    /**
+     * Step 1: Check Google Play services
+     */
+    private void checkGooglePlayServices() {
+
+        //Check whether this user has installed Google play service which is being used by Location updates.
+        if (isGooglePlayServicesAvailable()) {
+
+            //Passing null to indicate that it is executing for the first time.
+            checkInternetConnection(null);
+
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.no_google_playservice_available, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    /**
+     * Step 2: Check & Prompt Internet connection
+     */
+    private Boolean checkInternetConnection(DialogInterface dialog) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (activeNetworkInfo == null || !activeNetworkInfo.isConnected()) {
+            promptInternetConnect();
+            return false;
+        }
+
+
+        if (dialog != null) {
+            dialog.dismiss();
+        }
+
+        //Yes there is active internet connection. Next check Location is granted by user or not.
+
+        if (checkPermissions()) { //Yes permissions are granted by the user. Go to the next step.
+            checkAppPermissions();
+        } else {  //No user has not granted the permissions yet. Request now.
+            requestPermissions();
+        }
+        return true;
+    }
+
+    /**
+     * Show A Dialog with button to refresh the internet state.
+     */
+    private void promptInternetConnect() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(R.string.title_alert_no_intenet);
+        builder.setMessage(R.string.msg_alert_no_internet);
+
+        String positiveText = getString(R.string.btn_label_refresh);
+        builder.setPositiveButton(positiveText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+                        //Block the Application Execution until user grants the permissions
+                        if (checkInternetConnection(dialog)) {
+
+                            //Now make sure about location permission.
+                            if (checkPermissions()) {
+
+                                //Step 2: Start the Location Monitor Service
+                                //Everything is there to start the service.
+                                checkAppPermissions();
+                            } else if (!checkPermissions()) {
+                                requestPermissions();
+                            }
+
+                        }
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    /**
+     * Step 3: Start the Location Monitor Service
+     */
+    private void checkAppPermissions() {
+
+        //And it will be keep running until you close the entire application from task manager.
+        //This method will executed only once.
+
+        if (!mAlreadyStartedService && text_response != null) {
+
+            text_response.setText(R.string.msg_location_service_started);
+
+            //Start location sharing service to app server.........
+//            Intent intent = new Intent(this, LocationMonitoringService.class);
+//            startService(intent);
+//
+//            mAlreadyStartedService = true;
+            //Ends................................................
+        }
+    }
+
+    /**
+     * Return the availability of GooglePlayServices
+     */
+    public boolean isGooglePlayServicesAvailable() {
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int status = googleApiAvailability.isGooglePlayServicesAvailable(this);
+        if (status != ConnectionResult.SUCCESS) {
+            if (googleApiAvailability.isUserResolvableError(status)) {
+                googleApiAvailability.getErrorDialog(this, status, 2404).show();
+            }
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * Return the current state of the permissions needed.
+     */
+    private boolean checkPermissions() {
+        int permissionState1 = ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        int permissionState2 = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        return permissionState1 == PackageManager.PERMISSION_GRANTED && permissionState2 == PackageManager.PERMISSION_GRANTED;
+
+    }
+
+    /**
+     * Start permissions requests.
+     */
+    private void requestPermissions() {
+
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        boolean shouldProvideRationale2 =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION);
+
+
+        // Provide an additional rationale to the img_user. This would happen if the img_user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale || shouldProvideRationale2) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+            showSnackbar(R.string.permission_rationale,
+                    android.R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // Request permission
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                                    REQUEST_PERMISSIONS_REQUEST_CODE);
+                        }
+                    });
+        } else {
+            Log.i(TAG, "Requesting permission");
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the img_user denied the permission
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+
+    /**
+     * Shows a {@link Snackbar}.
+     *
+     * @param mainTextStringId The id for the string resource for the Snackbar text.
+     * @param actionStringId   The text of the action item.
+     * @param listener         The listener associated with the Snackbar action.
+     */
+    private void showSnackbar(final int mainTextStringId, final int actionStringId,
+                              View.OnClickListener listener) {
+        Snackbar.make(
+                findViewById(android.R.id.content),
+                getString(mainTextStringId),
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(actionStringId), listener).show();
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        Log.i(TAG, "onRequestPermissionResult");
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length <= 0) {
+                // If img_user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.");
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Log.i(TAG, "Permission granted, updates requested, starting location updates");
+                checkAppPermissions();
+
+            } else {
+                // Permission denied.
+
+                // Notify the img_user via a SnackBar that they have rejected a core permission for the
+                // app, which makes the Activity useless. In a real app, core permissions would
+                // typically be best requested during a welcome-screen flow.
+
+                // Additionally, it is important to remember that a permission might have been
+                // rejected without asking the img_user for permission (device policy or "Never ask
+                // again" prompts). Therefore, a img_user interface affordance is typically implemented
+                // when permissions are denied. Otherwise, your app could appear unresponsive to
+                // touches or interactions which have required permissions.
+                showSnackbar(R.string.permission_denied_explanation,
+                        R.string.settings, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Build intent that displays the App settings screen.
+                                Intent intent = new Intent();
+                                intent.setAction(
+                                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package",
+                                        BuildConfig.APPLICATION_ID, null);
+                                intent.setData(uri);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        });
+            }
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+
+
+        //Stop location sharing service to app server.........
+
+//        stopService(new Intent(this, LocationMonitoringService.class));
+//        mAlreadyStartedService = false;
+        //Ends................................................
+
+
+        super.onDestroy();
+    }
+
+
+    private void startStopService(boolean isChecked) {
+        if(isChecked){
+
+            //And it will be keep running until you close the entire application from task manager.
+            //This method will executed only once.
+
+            if (!mAlreadyStartedService && text_response != null) {
+
+                text_response.setText(R.string.msg_location_service_started);
+
+                //Start location sharing service to app server.........
+                Intent intent = new Intent(this, LocationMonitoringService.class);
+                startService(intent);
+
+                mAlreadyStartedService = true;
+                //Ends................................................
+            }
+
+        } else {
+            //Stop location sharing service to app server.........
+
+            stopService(new Intent(this, LocationMonitoringService.class));
+
+            mAlreadyStartedService = false;
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/pritam/fiberbaselocationtracker/services/LocatioUpdateWebservice.java
@@ -0,0 +1,104 @@
+package com.pritam.fiberbaselocationtracker.services;
+
+import android.content.Context;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+// https://stackoverflow.com/questions/28535703/best-way-to-get-user-gps-location-in-background-in-android
+
+
+class LocatioUpdateWebservice {
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public LocatioUpdateWebservice(Location location) {
+
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd-HH.mm.ss.SSS").format(new Date());
+       // ArrayList<HashMap<String, Object>> body = new ArrayList<>();
+        HashMap<String, Object> hm = new HashMap<>();
+        hm.put("Longitude", location.getLongitude());
+        hm.put("Latitude", location.getLatitude());
+        hm.put("Accuracy", location.getAccuracy());
+        hm.put("Altitude", location.getAltitude());
+        hm.put("Provider", location.getProvider());
+        hm.put("Speed", location.getSpeed());
+//        hm.put("SpeedAccuracyMetersPerSecond", location.getSpeedAccuracyMetersPerSecond());
+        hm.put("TimeStamp", timeStamp);
+       // body.add(hm);
+        timeStamp = new SimpleDateFormat("yyyy_MM_dd").format(new Date());
+
+        Gson gson = new Gson();
+        http_post_request(gson.toJson(hm), timeStamp);
+
+    }
+
+    private void http_post_request(String postBody, String date) {
+        try {
+            String url = "https://angular-db-fa163.firebaseio.com/locationtrack/"+date+".json";
+
+            MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+            RequestBody body = RequestBody.create(JSON, postBody);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .writeTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+            Log.d("-->>", request.toString());
+
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    call.cancel();
+                    Log.d("-->>", getStackTrace(e));
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    Log.d("-->>", response.toString());
+                }
+            });
+        } catch (Exception e) {
+            Log.d("-->>", getStackTrace(e));
+        }
+    }
+
+    public String getStackTrace(Throwable aThrowable) {
+        final Writer result = new StringWriter();
+        final PrintWriter printWriter = new PrintWriter(result);
+        aThrowable.printStackTrace(printWriter);
+        return result.toString();
+    }
+
+}
